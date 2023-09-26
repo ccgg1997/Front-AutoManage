2c4d5e81427d804f9e636b7270ace2a347d5054d
JoseM-18 <91767929+JoseM-18@users.noreply.github.com>
2023-09-26 15:00:35 -0500

    adicion de componente hero a la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
 import Navbar from './components/Navbar'
 import './App.css'
+import Hero from './components/Hero'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='App'>
        <Navbar />
-    </>
+       <Hero />
+    </div>
   )
 }
 
