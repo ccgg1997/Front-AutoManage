eea5f24f6893b1f18b0267cadea3ddbdda449387
JoseM-18 <91767929+JoseM-18@users.noreply.github.com>
2023-09-26 15:10:57 -0500

    constant y assets

---
+++ src/constant/index.js
@@ -0,0 +1,88 @@
+import {
+  model3,
+  modelS,
+  modelX,
+  modelY,
+  solarPanel,
+  solarRoof,
+  accessories,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "modelM",
+    title: "Model M",
+  },
+  {
+    id: "modelJ",
+    title: "Model J",
+  },
+  {
+    id: "modelV",
+    title: "Model V",
+  },
+  {
+    id: "modelA",
+    title: "Model A",
+  },
+  {
+    id: "modalC",
+    title: "modalC",
+  },
+  {
+    id: "solarPanels",
+    title: "Solar Panels",
+  },
+];
+
+export const hero = [
+  {
+    id: "modelM",
+    img: "../assets/images/Default_A_carousel_of_photos_for_a_car_dealership_featuring_ne_1.jpg",
+    title: "Model 3",
+    desc: "Leasing starting at $349/mo",
+    button: ["Custom Order", "Demo Drive"],
+  },
+  {
+    id: "modelY",
+    img: modelY,
+    title: "Model Y",
+    desc: "Order Online for Touchless Delivery",
+    button: ["Custom Order", "Demo Drive"],
+  },
+  {
+    id: "modelS",
+    img: modelS,
+    title: "Model S",
+    desc: "Order Online for Touchless Delivery",
+    button: ["Custom Order", "View Inventory"],
+  },
+  {
+    id: "modelX",
+    img: modelX,
+    title: "Model X",
+    desc: "Order Online for Touchless Delivery",
+    button: ["Custom Order", "View Inventory"],
+  },
+  {
+    id: "solarPanels",
+    img: solarPanel,
+    title: "Solar Panels",
+    desc: "Lowest Cost Solar Panels in America",
+    button: ["Order Now", "Learn More"],
+  },
+  {
+    id: "solarRoof",
+    img: solarRoof,
+    title: "Solar Roof",
+    desc: "Produce Clean Energy From Your Roof",
+    button: ["Order Now", "Learn More"],
+  },
+  {
+    id: "accessories",
+    img: accessories,
+    title: "Accessories",
+    desc: "",
+    button: ["Shop More", "Order Now"],
+  },
+];
