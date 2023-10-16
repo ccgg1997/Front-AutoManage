90d3e51a3611a808438857f456789c97575d038c
Cristian Gómez <89625031+ccgg1997@users.noreply.github.com>
2023-10-16 13:04:43 -0500

    Login call

--- src/data/data.js
+++ src/data/data.js
@@ -1,12 +1,11 @@
-const imagePathPrefix = "/public/";
 const imagePaths = [
-  "image1.jpg",
-  "image2.jpg",
-  "image3.jpg",
-  "image4.jpg",
-  "image5.jpg", 
-  "image6.jpg",
-  "image7.jpg",
+  "/image1.jpg",
+  "/image2.jpg",
+  "/image3.jpg",
+  "/image4.jpg",
+  "/image5.jpg", 
+  "/image6.jpg",
+  "/image7.jpg",
 ];
 
 
@@ -41,49 +40,49 @@ export const hero = [
   
   {
     id: "modalC",
-    img: imagePathPrefix + imagePaths[4],
+    img: imagePaths[4],
     title: "modal C",
     desc: "Lowest Cost Solar Panels in America",
     button: ["Order Now", "Learn More"],
   },
   {
     id: "modelM",
-    img: imagePathPrefix + imagePaths[0],
+    img: imagePaths[0],
     title: "Model M",
     desc: "Leasing starting at $349/mo",
     button: ["Custom Order", "Demo Drive"],
   },
   {
     id: "modelJ",
-    img: imagePathPrefix + imagePaths[1],
+    img: imagePaths[1],
     title: "Model Y",
     desc: "Order Online for Touchless Delivery",
     button: ["Custom Order", "Demo Drive"],
   },
   {
     id: "model A",
-    img: imagePathPrefix + imagePaths[2],
+    img: imagePaths[2],
     title: "model J",
     desc: "Order Online for Touchless Delivery",
     button: ["Custom Order", "View Inventory"],
   },
   {
     id: "modelV",
-    img: imagePathPrefix + imagePaths[3],
+    img: imagePaths[3],
     title: "model V",
     desc: "Order Online for Touchless Delivery",
     button: ["Custom Order", "View Inventory"],
   },
   {
     id: "solarRoof",
-    img: imagePathPrefix + imagePaths[5],
+    img: imagePaths[5],
     title: "Solar Roof",
     desc: "Produce Clean Energy From Your Roof",
     button: ["Order Now", "Learn More"],
   },
   {
     id: "accessories",
-    img: imagePathPrefix + imagePaths[6],
+    img: imagePaths[6],
     title: "Accessories",
     desc: "",
     button: ["Shop More", "Order Now"],
--- src/pages/Venta.jsx
+++ src/pages/Venta.jsx
@@ -3,6 +3,7 @@ const Venta = () => {
   return (
     <div>
         <h1>CONTENIDO VENTA</h1>
+        <img src="/image1.jpg"/>
     </div>
   )
 }
