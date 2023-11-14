2517753cc55b03789662d7d13154494ef46e1744
cgomez <cgomez@truora.com>
2023-11-13 20:35:43 -0500

    inventario fake

--- src/pages/Inventario.jsx
+++ src/pages/Inventario.jsx
@@ -1,9 +1,18 @@
-import * as React from 'react';
-import Tabs from '../components/Tabs';
+import React from 'react';
+import Tabs from '../components/Tabs.jsx';
+import TableEditDelete from '../components/TableEditDelete.jsx'; // Asegúrate de que la ruta sea correcta
+import { editItem, deleteItem } from './funciones.js'; // Asegúrate de que la ruta sea correcta
 
 const Inventario = () => {
+  // Datos de ejemplo para la tabla
+  const datosDeLaTabla = [
+    { companyName: 'KnobHome', country: 'Germany', status: 'deleted' },
+    { companyName: 'Squary', country: 'Sweden', status: 'active' },
+    { companyName: 'ghome', country: 'Switzerland', status: 'inactive' },
+  ];
+
   const tabs = [
-    { label: 'Repuestos', content: 'Contenido del Item One' },
+    { label: 'Repuestos', content: <TableEditDelete data={datosDeLaTabla} editFunc={editItem} deleteFunc={deleteItem} /> },
     { label: 'Vehiculo', content: 'Contenido del Item Two' },
     { label: 'Taller', content: 'Contenido del Item Three' },
   ];
---
+++ src/pages/funciones.js
@@ -0,0 +1,6 @@
+export const editItem = (item) => {
+    console.log(item+ "editado");
+    }
+export const deleteItem = (item) => {
+    console.log(item+ "eliminado");
+    }
