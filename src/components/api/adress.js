888b0a8a36cf4efdd1637f565a05370be0fafa74
ValSua <109840298+ValSua@users.noreply.github.com>
2023-11-15 22:51:53 -0500

    la tabla usuarios ahora me trae datos de los usuarios

--- src/components/api/adress.js
+++ src/components/api/adress.js
@@ -228,18 +228,19 @@ export const userUpdate = async (id,user, useToken) => {
   }
 }
 
-export const getUsuarios = async () => {
-  const usuariosAdress = apiAddress + "/usuarios";
+export const getUsuarios = async (useToken) => {
+  const usuariosAdress = apiAddress + "/usuarios/";
   try {
     const response = await axios.get(usuariosAdress,{
       headers: {
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk3NDkwNjM3LCJpYXQiOjE2OTc0ODg4MzcsImp0aSI6IjQ1OGQ2NDUzYTgwMzQxZTRhMGQzNmQ0YTRiN2U1MmRhIiwidXNlcl9pZCI6MiwidXNlcl9lbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInVzZXJfbm9tYnJlIjoiYWRtaW5pc3RyYWRvciIsInVzZXJfYXBlbGxpZG8iOiIiLCJ1c2VyX3JvbCI6IkplZmVfVGFsbGVyIn0.TEaS6f7qF4w0JAcdJsp-GAWSE6G9VMnGiFCuJAeIRGs",
+        Authorization: "Bearer " + useToken,
       },
     });
 
     return response.data
   } catch (error) {
-    throw new Error("error al obtener los usuarios",error);
+    console.log(error);
+    throw new Error("error al obtener los usuarios", error);
   }
 
 }
--- src/pages/Usuarios.jsx
+++ src/pages/Usuarios.jsx
@@ -1,15 +1,18 @@
 import { getUsuarios } from '../components/api/adress';
 import BasicTable from '../components/Table';
 import { useState } from 'react';
+import { useSelector } from "react-redux";
 
 const Usuarios = () => {
     const [data, setData] = useState([]);
   
     const titles = ["id_usuario", "nombre", "password", "estado", "id_rol"];
   
+    const { token } = useSelector((state) => state.auth);
+
     const onClick = async () => {
       try {
-        const response = await getUsuarios();
+        const response = await getUsuarios(token);
         setData(response);
       } catch (error) {
         console.log(error);
